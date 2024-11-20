e86a7d760167e27442aedfb2b92ad30be705f60f
Hema334 <112267828+Hema334@users.noreply.github.com>
2024-11-20 20:44:29 +0530

    Add files via upload

---
+++ object.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+class mca{
+     String str="Laptop";
+}
+class num {
+    int num=44;
+}
+ class Main{
+    String str1="Lenovo";
+    int n=4;
+    public static void main(String[]args){
+        Main obj = new Main();
+        System.out.println(obj.str1);
+        num obj1=new num();
+        System.out.println(obj1.num);
+        mca obj2=new mca();
+        System.out.println(obj2.str);
+    }
+}
